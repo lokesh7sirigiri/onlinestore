20b98913ba17d3f06462249f9ce0871c1c9015bb
lokesh7sirigiri <lokesh7.sirigiri@gmail.com>
2023-01-10 06:08:16 +0000

    first commit

---
+++ src/main/java/com/dummy/myapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.dummy.myapp.service.dto;
---
+++ src/main/java/com/dummy/myapp/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.dummy.myapp.service.mapper;
